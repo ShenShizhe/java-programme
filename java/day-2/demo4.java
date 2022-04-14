19bd1ef0c7d1528d9bb6b30bf24ba935fdcafa28
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-14 09:51:51 +0800

    Create demo4.java

---
+++ java/day-2/demo4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class demo4 {
+	public static void main(String[] args) {
+    
+		Scanner in=new Scanner(System.in);
+    
+		System.out.println("请输入身高(cm):");
+		double height=in.nextDouble();
+    
+		System.out.println("请输入体重(kg):");
+		double weight=in.nextDouble();
+    
+		double biaow=height-100;
+    
+		if(weight==biaow) {
+			System.out.println("您为标准体重");
+		}else {
+			if(weight-5>weight) 
+				System.out.println("您身体过胖");
+			else
+				System.out.println("您身体过瘦");
+		}
+		
+	}
+}
