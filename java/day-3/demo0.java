c4d2bc2c63a748d028ef9ab8d67465298c3ad1d7
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-14 10:12:58 +0800

    Create demo0.java

---
+++ java/day-3/demo0.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class demo0 {
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * 多分支
+		 * if(条件){
+		 * 
+		 * }else if(条件){
+		 * 
+		 * }else if(条件）{
+		 * ...
+		 * }else{}
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		//switch表达式中可以是整数、字符、字符串
+		
+		
+		
+		Scanner in=new Scanner(System.in);
+		System.out.println("请输入名次：");
+		int n=in.nextInt();
+		//switch判断
+		switch(n) {
+		case 1:
+			System.out.println("获得笔记本电脑");
+			break;
+		case 2:
+			System.out.println("获得移动硬盘");
+			break;
+		case 3:
+			System.out.println("获得U盘");
+			break;
+		default:
+			System.out.println("很遗憾，没有获得名次");
+			break;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
