6ac596bd414992b4dbdf398483a9473cf0e1dfa9
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-14 10:39:02 +0800

    Create demo7.java

---
+++ java/day-3/demo7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//Scanner in =new Scanner(System.in);
+public class Student {
+	public static void main(String[] args) {
+		//定义一个Student，sNo(学号)、sName(姓名)、sSex(性别)、sAge(年龄)、sJava()成绩
+		Scanner in=new Scanner(System.in);
+		System.out.println("请输入该学生的学号、姓名：");
+		String Sno=in.next(), sName=in.next();
+		System.out.println("请输入该学生的性别：");
+		char sSex=in.next().charAt(0);
+		System.out.println("请输入该学生的年龄：");
+		int sAge=in.nextInt();
+		System.out.println("请输入该学生的成绩：");
+		double sJava=in.nextDouble();
+		
+		
+		
+		
+		
+		
+		System.out.println();
+	}
+}
