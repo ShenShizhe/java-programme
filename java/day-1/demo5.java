057b7c81a14c6c620e951fa8465c59328d8480b1
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-14 09:25:11 +0800

    Create demo5.java

---
+++ java/day-1/demo5.java
@@ -0,0 +1,12 @@
+public class demo5 {
+	public static void main(String[] args) {
+		
+		int time=123456;
+		int hour,minute,second;
+		hour=time/(60*60);
+		minute=time%(60*60)/60;
+		second=time%60;
+		
+		System.out.println("123456秒是："+hour+"小时"+minute+"分钟"+second+"秒");
+	}
+}
