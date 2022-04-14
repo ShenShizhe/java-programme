9d594af5c8ec0e2506c5613ae307a612b698462f
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-14 09:21:29 +0800

    Create demo3.java

---
+++ java/day-1/demo3.java
@@ -0,0 +1,9 @@
+public class demo3 {
+	public static void main(String[] args) {
+		
+		double length=2.5;
+		double width=0.3;
+		System.out.println("长方形的周长为："+(length+width)*2+"米");
+		System.out.println("长方形的面积为："+length*width+"平方米");
+	}
+}
