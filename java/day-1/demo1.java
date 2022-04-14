390e44aa76f4909768ce16bd8ec5e9bade995eca
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-14 09:18:13 +0800

    Create demo1.java

---
+++ java/day-1/demo1.java
@@ -0,0 +1,13 @@
+public class demo1 {
+	public static void main(String[] args) {
+		String name1="iphone6";
+		String name2="小米5";
+		double price1=5400.0;
+		double price2=1900.0;
+		int quantity1=10;
+		int quantity2=8;
+		System.out.println("商品名称\t价格\t数量\t");
+		System.out.println(name1+"\t"+price1+"\t"+quantity1);
+		System.out.println(name2+"\t"+price2+"\t"+quantity2);	
+  }
+}
