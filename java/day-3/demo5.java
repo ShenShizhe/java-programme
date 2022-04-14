4ec433835d6984c432a47f6aa6a1875ab8ec4b18
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-14 10:30:58 +0800

    Create demo5.java

---
+++ java/day-3/demo5.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class demo5 {
+	
+	public static void main(String[] args) {
+//循环的要素
+		/*1.循环变量的初始值			例：打印36张试卷
+		 * 2.循环的条件				i=1;
+		 * 3.循环变量的自增或者自减		i<=36
+		 * 4.循环的操作				i++
+		 * 						system.out.print()
+		 */
+	
+		//while循环
+//		int i=1;
+//		while(i<=36) {
+//			System.out.println("打印第"+i+"张试卷");
+//			i++;
+//		}
+		
+		
+		int sum=0,i=2;
+		while(i<=100) {
+			sum+=i;			
+			i+=2;
+		}
+		System.out.println(sum);
+	}
+	
+	
+}
