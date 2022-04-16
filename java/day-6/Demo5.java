350dfac372e6b4793e7528677c4cce9dcd83f4bb
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-16 14:20:22 +0800

    Update Demo5.java

--- java/day-6/Demo5.java
+++ java/day-6/Demo5.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Demo5 {
-	//3、现在定义如下的一个数组：int oldArr[]={1,3,4,5,0,0,6,6,0,5,4,7,6,7,0,5},要求将以上数组中不为0的值存入一个新的数组
+	//现在定义如下的一个数组：int oldArr[]={1,3,4,5,0,0,6,6,0,5,4,7,6,7,0,5},要求将以上数组中不为0的值存入一个新的数组
 	
 	public static void main(String[] args) {
 		
