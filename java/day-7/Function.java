e565b292685cef3737c018c84dcfad1ce2e5357c
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-16 14:38:26 +0800

    Rename java/day-7/practice/src/Function.java to java/day-7/Function.java

--- java/day-7/Function.java
+++ java/day-7/Function.java
@@ -3,16 +3,16 @@ public class Function {
 	public void root(int a,int b,int c) {
 		int  detal=b*b-4*a*c;
 		if(detal<0)
-			System.out.println("ÎÞÊµÊý½â");
+			System.out.println("æ— å®žæ•°è§£");
 		else if(detal==0) {
 			double x=-b/(2*a);
-			System.out.println("ÓÐÇÒÖ»ÓÐÓÐÒ»¸öÊµÊý½â\t£º"+x);
+			System.out.println("æœ‰ä¸”åªæœ‰æœ‰ä¸€ä¸ªå®žæ•°è§£\tï¼š"+x);
 		}
 			
 		else {
 			double x1=(-b+Math.sqrt(detal)/(2*a));
 			double x2=(-b+Math.sqrt(detal)/(2*a));
-			System.out.println("ÓÐÁ½¸ö½â\tx1:"+x1+"\tx2:"+x2);
+			System.out.println("æœ‰ä¸¤ä¸ªè§£\tx1:"+x1+"\tx2:"+x2);
 			
 		}
 	}
