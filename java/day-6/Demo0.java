95a8a5ff01b7f0b72d99ee4411b8ea1849c28771
ShizheShen <94941582+ShenShizhe@users.noreply.github.com>
2022-04-16 13:56:21 +0800

    Update and rename java/day-6/practice/src/demo0.java to java/day-6/Demo0.java

---
+++ java/day-6/Demo0.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Demo0 {
+	
+	public static void main(String[] args) {
+		//声明一个长度为5的整型数组
+		int [] a;//操作1
+		a=new int[5];//操作2
+		
+		//通常将操作1与2合并一起写
+		int [] b=new int[5];
+		
+		//数组的数据类型和变量是一致的
+		double [] c=new double[10];
+		String [] str=new String[5];
+		char [] d=new char [8];
+		
+		//在声明数组的时候同时给数组赋值
+		int [] a1= {1,2,3,4,5};
+		System.out.println("a1的第一个元素是："+a1[0]);
+		System.out.println("a1的第四个元素是："+a1[3]);
+//		System.out.println("a1的第六个元素是："+a1[5]);
+		
+		//从键盘上为数组赋值
+		Scanner in=new Scanner(System.in);
+		
+		int [] a2=new int [5];
+		for (int i = 0; i < a2.length; i++) {
+			a2[i]=in.nextInt();
+		}
+		//输出
+		for (int i = 0; i < a2.length; i++) {
+			System.out.println(a2[i]);
+		}
+	}
+}
